b4f04621f871c159edf876944e819708e1b0081f
oussamasic <oussamazerouali42@gmail.com>
2024-02-15 10:55:53 +0100

    clean code

--- dev-deployment/docker/mongo/initialize_mongo_db.sh
+++ dev-deployment/docker/mongo/initialize_mongo_db.sh
@@ -1,7 +1,7 @@
 
 echo "Execute scripts to initialize the database"
 
-mongo --shell init-mongodb/01_initialize_database_user.js
-mongo --shell init-mongodb/02_create_collections.js
+mongosh --shell init-mongodb/01_initialize_database_user.js
+mongosh --shell init-mongodb/02_create_collections.js
 
-echo "Mongod service is started and the database is initialized"
+echo "Mongo service is started and the database is initialized"
